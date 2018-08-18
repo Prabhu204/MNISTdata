250d5fd48f63b925ba863b6cdd0f2a0791381381
searchtalent@paul <prabhu.appalapuri@gmail.com>
2018-08-18 15:54:37 +0200

    initial commit

--- Practice/scrach_files.py
+++ Practice/scrach_files.py
@@ -1,63 +1,11 @@
 """
 author: Prabhu
-email: prabhu.appalapuri@gmail.com
 """
-"""
-Linear Regression:
-    y = Ax+B
-"""
-# For example, we have car company. If the car price is low, we sell more car. If the car price is
-# high, we sell less car. This is the fact that we know and we have data set about this fact.
-# The question is that what will be number of car sell if the car price is 100.
-
-import numpy as np
-import torchvision
-import torch
-from torch.autograd import Variable
-
-
-# car_prices_array = [3,4,5,6,7,8,9]
-# car_price_np = np.array(car_prices_array,dtype=np.float32)
-# print(car_price_np)
-# print(np.shape(car_price_np))
-# car_price_np = car_price_np.reshape(-1,1)
-# print(car_price_np)
-# number_of_car_sell_array = [ 7.5, 7, 6.5, 6.0, 5.5, 5.0, 4.5]
-# number_of_car_sell_np = np.array(number_of_car_sell_array,dtype=np.float32)
-# number_of_car_sell_np = number_of_car_sell_np.reshape(-1,1)
-#
-# car_prices_tensor = variable(torch.from_numpy(car_price_np))
-# number_of_car_sell_tensor = variable(torch.from_numpy(number_of_car_sell_np))
-#
-# # lets visualize our data
-#
-# import matplotlib.pyplot as plt
-# plt.scatter(car_prices_array,number_of_car_sell_array)
-# plt.xlabel("Car Price $")
-# plt.ylabel("number of sellings")
-# plt.title("car vs sell")
-# plt.show()
-
-# import torch.nn as nn
-# import torch
-# x = torch.Tensor(2,3)
-
-# autograd in pycharm
-# from torch.autograd import Variable
-# from torch import autograd
-#
-# x = Variable(torch.ones(2,2)*2, requires_grad= True)
-# print(x)
-#
-# z = 2*(x*x)+ 5*x
-# print(z)
-#
-# z.backward(torch.ones(2,2))
-# print(x.grad)
 
 #creating a neural network in pytorch
 
 import torch
+from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 from torch import optim
@@ -65,38 +13,50 @@ from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 import torchvision.datasets
 
+def create_nn(batch_size = 100, learning_rate = 0.1, epochs= 20, log_interval = 10):
+
+    class Net(nn.Module):
+        def __init__(self):
+            super(Net, self).__init__()
+            self.fc1 = nn.Linear(28*28, 200)
+            self.fc2 = nn.Linear(200,200)
+            self.fc3 = nn.Linear(200,10)
+
+        # we have to define how data flows through out network
+
+        def forward(self, x):
+            x = F.relu(self.fc1(x))
+            x = F.relu(self.fc2(x))
+            x = F.fc3(x)
+            return F.log_softmax(x)
+
+    net = Net()
+    print(net)
 
 
+    # training the network
+    # Next we have to setup an optimizer and a loss criterion:
+    # crate a stochastic gradient decent optimizer
+    optimizer = optim.SGD(net.parameters(), lr = learning_rate, momentum= 0.9)
+    # crate a loss function
+    criterion = nn.NLLLoss()
+    # run the main training loop
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(28*28, 200)
-        self.fc2 = nn.Linear(200,200)
-        self.fc3 = nn.Linear(200,10)
+    for epoch in range( epochs):
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data, target = Variable(data), Variable(target) # converting data into Pytorch variables
+            # resize data from (batch_size,1,28,28) to (batch_size, 28*28)
+            data = data.view(-1,28*28)  # applying reshape to the function for single dimension
+            optimizer.zero_grad()   # to reset all gradients in the model
+            # net_out = net(data)  #
+            # loss = criterion(net_out, target)
+            loss.backward()
+            optimizer.step()
+        if batch_idx % log_interval ==0:
+            print('Train Epoch: {} [{}/{} ({:0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx*len(data),len(train_loader.dataset),
+            100. *batch_idx/len(train_loader), loss.data[0]))
 
-# we have to define how data flows through out network
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.fc3(x)
-        return F.log_softmax(x)
-
-
-net = Net()
-print(net)
-
-
-# training the network
-# Next we have to setup an optimizer and a loss criterion:
-# crate a stochastic gradient decent optimizer
-optimizer = optim.SGD(net.parameters(), lr = 0.1, momentum= 0.9)
-# crate a loss function
-criterion = nn.NLLLoss()
-# run the main training loop
-
-for epoch in range( epochs):
 
 
 
